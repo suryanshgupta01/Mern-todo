0016d32a4b9e65206a72c4ea108827dfc244a93f
suryansh <suryanshgupta03@gmail.com>
2023-06-05 14:42:44 +0530

    added edit feature

--- api/server.js
+++ api/server.js
@@ -19,22 +19,27 @@ mongoose.connect('mongodb://127.0.0.1:27017/mern-todo-yt', {
 app.listen(port, () => console.log(`server started at ${port}`))
 
 app.get('/todos', async (req, res) => {
-    const todos = await Todo.find()
-    res.json(todos)
+    const todos = await Todo.find().sort({ timestamp: -1 })
+    res.status(200).json(todos)
 })
 
 app.post('/todo/new', (req, res) => {
     const todo = new Todo({
-        text: req.body.text,
-        complete: req.body.complete
+        text: req.body.text
+        // complete: req.body.complete
     });
-    todo.save()
+    todo.save();
     res.json(todo);
 })
 
 app.delete('/todo/delete/:id', async (req, res) => {
-    const result = await Todo.findByIdAndDelete(req.params.id);
-    res.json(result);
+    try {
+        const result = await Todo.findByIdAndDelete(req.params.id);
+        res.json(result);
+    } catch (error) {
+        res.status(404).send(error)
+    }
+
 })
 
 //chatgpt recommended below
@@ -52,10 +57,38 @@ app.delete('/todo/delete/:id', async (req, res) => {
 //   });
 
 
-app.get('/todo/complete/:id',async(req,res)=>{
+app.get('/todo/complete/:id', async (req, res) => {
     const todo = await Todo.findById(req.params.id);
     todo.complete = !todo.complete;
     todo.save();
     res.json(todo);
 })
 
+//patch command to update json using id
+// app.patch('/todo/update/:id', async (req, res) => {
+//     const todo = await Todo.findByIdAndUpdate(req.params.id, "req.body",
+//         { new: true },
+//         (err, todo) => {
+//             if (err) {
+//                 console.log(err);
+//             } else {
+//                 console.log(todo);
+//             }
+//         });
+//     // todo.complete = req.body.complete;
+//     // todo.text = req.body.text;
+//     todo.save();
+//     res.json(todo);
+// })     
+
+app.patch('/todo/update/:id', async (req, res) => {
+    try {
+        const reqid = req.params.id;
+        const data = await Todo.findByIdAndUpdate({ _id: reqid }, req.body, { new: true })
+        // data.text = req.body.text
+        // data.save()
+        res.status(200).send(data)
+    } catch (error) {
+        res.status(500).send(error)
+    }
+})     
--- client/src/App.js
+++ client/src/App.js
@@ -5,13 +5,18 @@ function App() {
   const [todos, setTodos] = useState([]);
   const [popupactive, setpopupactive] = useState(false);
   const [newtodo, setnewtodo] = useState("");
+  const [updatedtodo, setupdatedtodo] = useState("");
+  const [popupdate, setpopupdate] = useState(false);
+  const [idtoupdate, setidtoupdate] = useState(0);
 
   useEffect(() => {
     fetch("http://localhost:4000/todos")
       .then((res) => res.json())
-      .then((data) => setTodos(data))
+      .then((data) => {
+        // console.log(data)
+        setTodos(data)
+      })
 
-    console.log(todos)
   }, []);
 
   const completeTodo = async (id) => {
@@ -25,7 +30,7 @@ function App() {
       return todo;
     }))
   }
-  
+
   const deleteTodo = async (id) => {
     const data = await fetch("http://localhost:4000/todo/delete/" + id, {
       method: "DELETE"
@@ -33,14 +38,87 @@ function App() {
     setTodos(todos => todos.filter(todo => todo._id !== data._id))
   }
 
-  // const deleteTodo = async id => {
-	// 	const data = await fetch(api_base + '/todo/delete/' + id, { method: "DELETE" }).then(res => res.json());
+  //make a put request to edit todo
+  const updatetodo = async () => {
+    // console.log(updatedtodo)
+    const id = idtoupdate
+    completeTodo(id) //have to toggle coz it was acting as we clicked the todo
+    // console.log(id)
+    setTodos(todos.map((elem) => {
+      if (elem._id === id) {
+        elem.text = `${updatedtodo}`
+      }
+      return elem
+    }))
 
-	// 	setTodos(todos => todos.filter(todo => todo._id !== data.result._id));
-	// }
+    // todos.save()
+    fetch("http://localhost:4000/todo/update/" + id, {
+      method: 'PATCH',
+      body: JSON.stringify({
+        text: `${updatedtodo}`
+      }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    })
+      .then((response) => response.json())
+    // .then((json) => console.log(json));
+
+    setupdatedtodo("")
+    setpopupdate(false)
+    // console.log(data)
+    // console.log(updatedtodo)
+    // const data = await fetch("http://localhost:4000/todo/update/" + id, {
+    //   method: "PUT",
+    //   headers: {
+    //     "Content-Type": "application/json"
+    //   },
+    //   body: JSON.stringify({
+    //     text: updatedtodo
+    //   })
+    // }).then((res) => res.json())
+
+    // setTodos([...todos, data])
+
+
+    // .then((data) => console.log(data))
+    // const deleteTodo = async id => {
+    // 	const data = await fetch(api_base + '/todo/delete/' + id, { method: "DELETE" }).then(res => res.json());
+
+    // 	setTodos(todos => todos.filter(todo => todo._id !== data.result._id));
+    // }
+
+    // const updatetodo = async (id) => {
+    //   const data = await fetch("http://localhost:4000/todo/update/" + id, {
+    //     method: "PUT",
+    //     headers: {
+    //       "Content-Type": "application/json"
+    //     },
+    //     body: JSON.stringify({
+    //       text: updatedtodo 
+    //     })
+    //   }).then((res) => res.json())
+    //   setTodos([...todos, data])
+    //   setupdatedtodo("")
+    //   setpopupdate(false)
+    //   console.log(data)
+    //   console.log(updatedtodo)
+    //   // console.log(res)
+    //   // console.log(body)
+    //   // setTodos(todos => todos.map(todo => {
+    //   //   if (todo._id === data._id) {
+    //   //     todo.complete = data.complete;
+    //   //   }
+    //   //   return todo;
+    //   // }))
+
+
+    // }
+
+  }
 
   const addtodo = async () => {
-    if (newtodo == "") {
+    if (newtodo === "") {
       alert("Please enter a task")
       return;
     }
@@ -52,14 +130,21 @@ function App() {
       },
       body: JSON.stringify({
         text: newtodo,
-        complete: false
+        complete: true
       })
     }).then((res) => res.json())
-    setTodos([...todos, data])
+    setTodos([data, ...todos]) //changed order to match latest todo at top
     setnewtodo("")
     setpopupactive(false)
 
   }
+  const updatefunction = (id) => {
+    setpopupdate(true);
+    setidtoupdate(id)
+    // setTimeout(() => {
+    //     updatetodo(id);
+    // }, 3000);
+  };
   return (
     <>
       <div className="app">
@@ -68,17 +153,41 @@ function App() {
         <div className="todos">
           {
             todos.map((todo) => (
+              <>
+                <div className=
+                  {"todo " + (todo.complete ? "iscomplete" : "")}
+                  key={todo._id} onClick={() => completeTodo(todo._id)}>
+                  <div className="checkbox"></div>
+                  <div className="text">{todo.text}</div>
+                  <div className="edittodo" onClick={() => updatefunction(todo._id)}>EDIT</div>
+                  <div className="delete" onClick={() => deleteTodo(todo._id)}>X</div>
+                </div>
+              </>
 
-              <div className=
-                {"todo " + (todo.complete ? "iscomplete" : "")}
-                key={todo._id} onClick={() => completeTodo(todo._id)}>
-                <div className="checkbox"></div>
-                <div className="text">{todo.text}</div>
-                <div className="delete" onClick={() => deleteTodo(todo._id)}>X</div>
-              </div>
-            ))}
+            )
+            )}
 
         </div>
+
+        {/* <div className="edittodo" onClick={() => setpopupdate(true)}>EDIT</div> */}
+        {
+          popupdate ? (
+            <div className="popup">
+              <div className="closepopup" onClick={() => setpopupdate(false)}>X</div>
+              <div className="content">
+                {/* hgszdghsfjjgsd-{updatedtodo} */}
+                <h3>edit task</h3>
+                <input
+                  type="text"
+                  className="addinput"
+                  placeholder="Enter your task"
+                  value={updatedtodo}
+                  onChange={e => setupdatedtodo(e.target.value)} />
+                <button className="button" onClick={() => updatetodo()}>EDIT task</button>
+              </div>
+            </div>
+          )
+            : ''}
         <div className="addtodo" onClick={() => setpopupactive(true)}>+</div>
         {
           popupactive ? (
